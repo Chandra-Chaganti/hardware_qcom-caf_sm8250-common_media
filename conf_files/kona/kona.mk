f08119a78b2319421fd40ac9ba7172e0219b6654
chrisw444 <wandersonrodriguesf1@gmail.com>
2022-10-21 16:42:33 +0530

    conf_files: kona: Update media profiles path according to device/qcom/common
    Change-Id: I5e51d30e7f52fbfe4eb8c347957c404c95ea4f57

--- conf_files/kona/kona.mk
+++ conf_files/kona/kona.mk
@@ -34,7 +34,7 @@ else
   $(warning "Enabling codec2.0 non-audio SW only for non-generic odm build variant")
 PRODUCT_VENDOR_PROPERTIES += debug.stagefright.omx_default_rank=0
   PRODUCT_COPY_FILES += \
-    device/qcom/common/media/media_profiles.xml:$(TARGET_COPY_OUT_ODM)/etc/media_profiles_V1_0.xml
+    device/qcom/common/vendor/media/media_profiles.xml:$(TARGET_COPY_OUT_ODM)/etc/media_profiles_V1_0.xml
 endif
 
 # Produce packages addition
